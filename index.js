00a24003e7e750f9985af3d37563a76f2a3195db
asooge <asooge@gmail.com>
2020-11-07 00:24:32 -0500

    subscribe to newBlockHeaders

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const web3 = new Web3(`ws://localhost:8545`)
 
 const port = process.env.PORT || 4000
 
+web3.eth.subscribe('newBlockHeaders', (error, result) => {
+  console.log(result.hash)
+})
+
 app.get('/', async (req, res) => {
   const accounts = await web3.eth.getAccounts()
   res.send(accounts)
