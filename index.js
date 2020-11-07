75e0ae7c18b774bd413dd040b5ed60971130768a
asooge <asooge@gmail.com>
2020-11-06 23:19:43 -0500

    Express server connected to web3

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const Web3 = require('web3')
+const web3 = new Web3(`ws://localhost:8545`)
+
+const port = process.env.PORT || 4000
+
+app.get('/', async (req, res) => {
+  const accounts = await web3.eth.getAccounts()
+  res.send(accounts)
+})
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/`)
+})
